217779016c129a05c7b0b6ee0721d6df08f785f3
longdo12 <gialong12.12.99@gmail.com>
2022-04-14 16:22:55 +0700

    Initial Build

--- models.py
+++ models.py
@@ -141,7 +141,7 @@ class construct_transformer_student_model(torch.nn.Module):
                 for i in range(args["num_hidden_layers"]+1):
                     self.hidden_dropout.append(torch.nn.Dropout(p=student_config.hidden_dropout_prob))
                     if i == 0:
-                        self.hidden_linear.append(torch.nn.Linear(348,args["teacher_hidden_size"]))
+                        self.hidden_linear.append(torch.nn.Linear(384,args["teacher_hidden_size"]))
                         torch.nn.init.trunc_normal_(self.hidden_linear[-1].weight, std= student_config.initializer_range)
                     else:
                         self.hidden_linear.append(torch.nn.Linear(args["teacher_hidden_size"],args["teacher_hidden_size"]))
